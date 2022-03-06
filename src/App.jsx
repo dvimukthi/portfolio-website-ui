9d9a1412d48d94b9383398f888feea9e2fbe5f46
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-07 03:50:12 +0530

    import Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Experience />
       <Services />
+      <Portfolio />
     </>
   );
 };
