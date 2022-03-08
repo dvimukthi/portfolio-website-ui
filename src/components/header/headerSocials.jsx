11535ee9fc8f7b2ecc7b6676c0a7be57f6aa2d8f
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-08 10:32:12 +0530

    github connect btn

--- src/components/header/headerSocials.jsx
+++ src/components/header/headerSocials.jsx
@@ -4,6 +4,7 @@ const headerSocials = () => {
   return (
     <div className="header__socials">
       <a href="https://linkedin.com" target={blank}></a>
+      <a href="https://github.com" target={blank}></a>
     </div>
   );
 };
