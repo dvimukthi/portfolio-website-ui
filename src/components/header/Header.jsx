42423e9af35efbe3c7b1ae7c2f5cc97367fe53de
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-08 09:39:02 +0530

    h5

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <div className="container header__container"></div>
+      <div className="container header__container">
+        <h5>hello I'm</h5>
+        <h1>Deshan Vimukthi</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+      </div>
     </header>
   );
 };
