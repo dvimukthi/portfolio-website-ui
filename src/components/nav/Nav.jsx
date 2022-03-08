39395ef77a0e89f55d4d1098fb99ec5fc1e29967
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-08 23:22:27 +0530

    HiOutlinePresentationChartLine

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Nav.css";
 import { TiHomeOutline } from "react-icons/ti";
 import { AiOutlineProfile } from "react-icons/ai";
+import { HiOutlinePresentationChartLine } from "react-icons/hi";
 
 const Nav = () => {
   return (
@@ -9,6 +10,7 @@ const Nav = () => {
       <a href="#">
         <TiHomeOutline />
         <AiOutlineProfile />
+        <HiOutlinePresentationChartLine />
       </a>
     </nav>
   );
