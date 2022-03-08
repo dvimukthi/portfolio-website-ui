3bd8ad590b362e4514ee3c19c6c0cb24f923f88c
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-08 22:14:38 +0530

    cta

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>hello I'm</h5>
+        <h5>Hello I'm</h5>
         <h1>Deshan Vimukthi</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
