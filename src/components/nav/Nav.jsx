46906d88cc9149add2f116e20a5c341ef03b12e0
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-08 23:18:04 +0530

    AiOutlineProfile icon

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Nav.css";
 import { TiHomeOutline } from "react-icons/ti";
+import { AiOutlineProfile } from "react-icons/ai";
 
 const Nav = () => {
   return (
     <nav>
       <a href="#">
         <TiHomeOutline />
+        <AiOutlineProfile />
       </a>
     </nav>
   );
