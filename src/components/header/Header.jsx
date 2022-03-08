4a929b5732c49fa504f8c7f622c2f8602e536a7b
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-08 09:35:23 +0530

    container header__container

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container"></div>
+    </header>
+  );
 };
 
 export default Header;
