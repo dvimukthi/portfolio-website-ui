35a62c323e3ecbfa8310ae924b471e1738984945
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-08 10:07:05 +0530

    import CV

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import CV from "../../assets/cv.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="">Download CV</a>
+      <a href={CV}>Download CV</a>
       <a href="#contact">Contact</a>
     </div>
   );
