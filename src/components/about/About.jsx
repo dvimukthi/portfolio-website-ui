f1f9168c6e64b20361af2f272d11c0ce27f9f5da
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-14 13:35:46 +0530

    About h5

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./About.css";
 const About = () => {
-  return <section id="About">About</section>;
+  return (
+    <section id="About">
+      <h5>Get You know</h5>
+      <h2>About Me</h2>
+    </section>
+  );
 };
 
 export default About;
