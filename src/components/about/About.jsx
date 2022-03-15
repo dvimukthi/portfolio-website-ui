2b15bc270a4578b531bdb91e29a3ba245e57db07
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-15 16:22:33 +0530

    about__me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,9 @@ const About = () => {
       <h5>Get You know</h5>
       <h2>About Me</h2>
 
-      <div className="container about__container"></div>
+      <div className="container about__container">
+        <div className="about__me"></div>
+      </div>
     </section>
   );
 };
