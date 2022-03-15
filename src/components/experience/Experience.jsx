76ed303d8995f69d7f3d01053746d585eb86b126
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-15 18:28:59 +0530

    Experience h5, h2

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./Experience.css";
 
 const Experience = () => {
-  return <section id="Experience">Experience</section>;
+  return (
+    <section id="Experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+    </section>
+  );
 };
 
 export default Experience;
