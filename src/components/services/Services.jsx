d539781c62c58a333ae35959cee9ad25e8cdb279
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 14:26:31 +0530

    h5

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Services.css";
 
 const Services = () => {
-  return <section id="Services">Services</section>;
+  return (
+    <section id="Services">
+      <h5>What I Offer</h5>
+    </section>
+  );
 };
 
 export default Services;
