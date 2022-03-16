4de0b96b1c0ec0f65f65541f760cfaf92872793f
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 15:55:41 +0530

    h5

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Portfolio.css";
 
 const Portfolio = () => {
-  return <section id="Portfolio">Portfolio</section>;
+  return (
+    <section id="Portfolio">
+      <h5>My Recent Work</h5>
+    </section>
+  );
 };
 
 export default Portfolio;
