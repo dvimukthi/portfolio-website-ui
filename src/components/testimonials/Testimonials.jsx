639871e81b838b9ab36da490c6117171a6b649d0
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 20:46:55 +0530

    article

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,7 +6,9 @@ const Testimonials = () => {
     <section id="Testimonials">
       <h5>Review From Clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials__container"></div>
+      <div className="container testimonials__container">
+        <article className="testimonials"></article>
+      </div>
     </section>
   );
 };
