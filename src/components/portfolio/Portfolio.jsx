e308706559f9df699a61d1dc75b5b3637153f784
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 15:58:18 +0530

    portfolio__item-image

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,7 +7,9 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className="portfolio__item"></article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+        </article>
       </div>
     </section>
   );
