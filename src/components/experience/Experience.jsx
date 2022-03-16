0057ef92a45fae67c7c3072f196e72216fbf23d5
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 13:15:49 +0530

    Javascript

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -22,6 +22,11 @@ const Experience = () => {
               <h4>CSS</h4>
               <small className="text-light">Experienced</small>
             </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill />
+              <h4>Javascript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
           </div>
         </div>
         <div className="experience__backend"></div>
