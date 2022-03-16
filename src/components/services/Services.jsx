38bd403d0d600a1508e5d2b1b63546176a4badb0
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 14:33:42 +0530

    ul

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -12,6 +12,7 @@ const Services = () => {
           <div className="services__head">
             <h3>UI/UX Design</h3>
           </div>
+          <ul className="service__list"></ul>
         </article>
       </div>
     </section>
