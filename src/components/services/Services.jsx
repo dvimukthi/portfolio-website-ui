ef01dab7a2bb38a47582c482c83d7e040264a81f
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 14:29:46 +0530

    services__head

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -8,7 +8,9 @@ const Services = () => {
       <h2>Services</h2>
 
       <div className="container services__container">
-        <article className="service"></article>
+        <article className="service">
+          <div className="services__head"></div>
+        </article>
       </div>
     </section>
   );
