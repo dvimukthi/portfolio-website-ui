749aa842b7c1b6aae32bc23d9561ad3fc4cf1419
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-16 16:04:22 +0530

    live demo

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,7 +10,15 @@ const Portfolio = () => {
         <article className="portfolio__item">
           <div className="portfolio__item-image"></div>
           <h3>Portfolio item title</h3>
-          <a href="https://github.com/dvimukthi/website-UI">Github</a>
+          <a href="https://github.com/dvimukthi/website-UI" className="btn">
+            Github
+          </a>
+          <a
+            href="https://github.com/dvimukthi/website-UI"
+            className="btn btn-primary"
+          >
+            Live Demo
+          </a>
         </article>
       </div>
     </section>
