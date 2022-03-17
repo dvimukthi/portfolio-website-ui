fd0835a93bfccdbc00ea82778010d7970fdb418f
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-17 15:35:28 +0530

    Email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,7 +8,10 @@ const Contact = () => {
       <h2>Contact Me</h2>
       <div className="container contact__container">
         <div className="contact__options">
-          <article className="contact__option"></article>
+          <article className="contact__option">
+            <h4>Email</h4>
+            <h5>example@gmail.com</h5>
+          </article>
         </div>
         <form action=""></form>
       </div>
