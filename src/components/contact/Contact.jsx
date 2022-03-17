98e855c3dd4f257dddf1f54ccafe9f8bea056747
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-17 15:48:35 +0530

    Instagram

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Contact.css";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsFacebook } from "react-icons/bs";
+import { FiInstagram } from "react-icons/fi";
 
 const Contact = () => {
   return (
@@ -19,8 +20,14 @@ const Contact = () => {
           <article className="contact__option">
             <BsFacebook />
             <h4>Facebook</h4>
-            <h5>Deshan Vimukthi/</h5>
+            <h5>Deshan Vimukthi</h5>
             <a href="https://www.facebook.com/">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <FiInstagram />
+            <h4>Instagram</h4>
+            <h5>Deshan</h5>
+            <a href="https://www.instagram.com/">Send a message</a>
           </article>
         </div>
         <form action=""></form>
