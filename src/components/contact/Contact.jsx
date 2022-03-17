575caab6cb02809571e580cb34fe1ca26e968dc8
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-03-17 15:29:10 +0530

    contact h5

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  return <section id="Contact">Contact</section>;
+  return (
+    <section id="Contact">
+      <h5>Get In Touch</h5>
+    </section>
+  );
 };
 
 export default Contact;
